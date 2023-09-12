cc95da1da623c179e3afac6104c57de5426a3d24
lszdp <1191739252@qq.com>
2023-09-12 15:42:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+src-git cdnspeedtest https://github.com/immortalwrt-collections/openwrt-cdnspeedtest.git
+src-git cdnspeedluci https://github.com/mingxiaoyu/luci-app-cloudflarespeedtest.git
 #echo 'src-git kenzok https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
